e29dcfb6476ac77aab14ab77cd24fbc5c2a3a928
jsmith440 <jsmith440@cnm.edu>
2020-01-24 11:29:36 -0700

    Completed UnitTest

--- src/edu/cnm/deepdive/model/Card.java
+++ src/edu/cnm/deepdive/model/Card.java
@@ -1,16 +1,18 @@
 package edu.cnm.deepdive.model;
 
+import java.util.Objects;
+
 public class Card {
 
   private final Suit suit;
   private final Rank rank;
+  private final int hash;
 
   public Card(Suit suit, Rank rank) {
     this.suit = suit;
     this.rank = rank;
+    hash = Objects.hash(suit, rank);
   }
-
-
 
   public Suit getSuit() {
     return suit;
@@ -23,6 +25,25 @@ public class Card {
   @Override
   public String toString() {
     return rank.symbol() + suit.symbol();
+  }
+
+  @Override
+  public int hashCode() {
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean comparison = false;
+    if (obj == this) {
+      comparison = true;
+    } else if (obj instanceof Card && obj.hashCode() == hash) {
+      Card other = (Card) obj;
+      if (hash == other.hash && suit == other.suit && rank == other.rank) {
+        comparison = true;
+      }
+      }
+    return comparison;
   }
 
 }
